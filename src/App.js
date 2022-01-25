df6b8e54354b112cb7a3d559acbdcd23dc769663
Temiloluwa-ade <temiadev@gmail.com>
2022-01-25 19:59:16 +0100

    Name

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import { IconGithub, IconLinkedin, IconsEmail, IconTwitter } from './icon';
 const App = () => {
   return <div className='hero'>
     <div className="left">
-      <h1>John Doe</h1>
-      <h3>Geographer and Videographer</h3>
+      <h1>Similoluwa Adelowo</h1>
+      <h3>Developer and Designer</h3>
       <p>
         <a href="/" target="_blank" rel='noppener noreferral'><IconsEmail /></a>
         <a href="/" target="_blank" rel='noppener noreferral'><IconLinkedin /></a>
