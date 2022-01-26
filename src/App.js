1370f4bf1296baf4dafff50251200d8195c2030f
Temiloluwa-ade <temiadev@gmail.com>
2022-01-26 11:35:39 +0100

    ..

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import React from 'react';
-import './App.css'
-import { IconGithub, IconLinkedin, IconsEmail, IconTwitter } from './icon';
+import React from "react";
+import "./App.css";
+import { IconGithub, IconLinkedin, IconsEmail, IconTwitter } from "./icon";
 
 const App = () => {
-  return <div className='hero'>
-    <div className="left">
-      <h1>Similoluwa Adelowo</h1>
-      <h3>Developer and Designer</h3>
-      <p>
-        <a href="/" target="_blank" rel='noppener noreferral'><IconsEmail /></a>
-        <a href="/" target="_blank" rel='noppener noreferral'><IconLinkedin /></a>
-        <a href="/" target="_blank" rel='noppener noreferral'><IconGithub /></a>
-        <a href="/" target="_blank" rel='noppener noreferral'><IconTwitter /></a>
-      </p>
+  return (
+    <div className="hero">
+      <div className="left">
+        <h1>Similoluwa Adelowo</h1>
+        <h3>Developer and Designer</h3>
+        <p>
+          <a href="/" target="_blank" rel="noppener noreferral">
+            <IconsEmail />
+          </a>
+          <a href="/" target="_blank" rel="noppener noreferral">
+            <IconLinkedin />
+          </a>
+          <a href="/" target="_blank" rel="noppener noreferral">
+            <IconGithub />
+          </a>
+          <a href="/" target="_blank" rel="noppener noreferral">
+            <IconTwitter />
+          </a>
+        </p>
+      </div>
+      <div className="right">
+        <img src="https://i.postimg.cc/vmrR8WB9/image.png" alt="Me" />
+      </div>
     </div>
-    <div className="right">
-      <img src="./picture.jpg" alt="Me" />
-    </div>
-  </div>;
+  );
 };
 
 export default App;
